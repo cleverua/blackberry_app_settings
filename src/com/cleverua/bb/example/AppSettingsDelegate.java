db75c943413bc9df6c1b123b0af12de5b5c90fc9
Vitaliy Khudenko <vitaliy.khudenko@gmail.com>
2010-02-19 13:21:31 +0200

    refactoring

--- src/com/cleverua/bb/example/AppSettingsDelegate.java
+++ src/com/cleverua/bb/example/AppSettingsDelegate.java
@@ -6,10 +6,12 @@ import com.cleverua.bb.settings.Utils;
 public class AppSettingsDelegate extends SettingsDelegate {
     private static final int VERSION = 1;
     public static final String SETTINGS_FILE = 
-        "file:///SDCard/your_application_dir/your_application.settings";
+        "file:///SDCard/your_application.settings";
     
     public static final String USER_TEXT_KEY = "userText";
     public static final String USER_CHOICE_KEY = "userChoice";
+    
+    private static final boolean USER_CHOICE_DEFAULT = true;
     
     public int version() {
         return VERSION;
@@ -20,7 +22,13 @@ public class AppSettingsDelegate extends SettingsDelegate {
     }
     
     public boolean getUserChoice() {
-        return Utils.stringToBoolean(getData().get(USER_CHOICE_KEY).toString());
+        Object read = getData().get(USER_CHOICE_KEY);
+        
+        if (read == null) {
+            return USER_CHOICE_DEFAULT;
+        }
+        
+        return /*Utils.stringToBoolean*/((Boolean) read).booleanValue();
     }
     
     public void setUserChoice(boolean userChoice) {
@@ -28,7 +36,7 @@ public class AppSettingsDelegate extends SettingsDelegate {
     }
     
     public String getUserText() {
-        return getData().get(USER_TEXT_KEY).toString();
+        return (String) getData().get(USER_TEXT_KEY);
     }
     
     public void setUserText(String userText) {
